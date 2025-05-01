9888b1448cc3f49aad61367b72da9006c423b320
samuelard7 <samuelrichard214@gmail.com>
2025-05-02 03:09:56 +0530

    Some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -10,7 +5,6 @@
 		$body = $('body'),
 		$nav = $('#nav');
 
-	// Breakpoints.
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
@@ -18,14 +12,12 @@
 			small:   [ null,      '736px'  ]
 		});
 
-	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-	// Scrolly.
 		$('#nav a, .scrolly').scrolly({
 			speed: 1000,
 			offset: function() { return $nav.height(); }
